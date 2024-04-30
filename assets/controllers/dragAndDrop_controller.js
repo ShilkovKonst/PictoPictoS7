a526f766f9e08bf7044b4cdb62962956c56e34da
Konstantin Shilkov <konst.shilkov@gmail.com>
2024-04-30 04:52:00 +0200

    dragndrop in progress

--- assets/controllers/dragAndDrop_controller.js
+++ assets/controllers/dragAndDrop_controller.js
@@ -1,57 +1,76 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  isSame = false;
+
   // for 'dragstart' and 'dragEnd' event on source (draggable)
-  dragPicto(e) {
-    // e.dataTransfer.effectAllowed = "copy";
+  dragStartPicto(e) {
+    if (e.target.parentNode.classList.contains("dropzone")) {
+      e.dataTransfer.setData(
+        "sourceParentId",
+        e.target.parentNode.dataset.number
+      );
+    }
     e.dataTransfer.setData("id", e.params.id);
     e.dataTransfer.setData("title", e.params.title);
     e.dataTransfer.setData("image", e.params.image);
     e.dataTransfer.setData("alt", e.params.alt);
-    // console.log(e.dataTransfer);
-    // console.log(e.target);
     e.dataTransfer.effectAllowed = "move";
-    console.log(e.dataTransfer.getData("source"));
   }
-  testPicto(e) {
-    // console.log(e.dataTransfer);
-    // console.log(e.target);
-    console.log("drag ends");
+  dragEndPicto(e) {
+    if (e.target.parentNode.classList.contains("dropzone")) {
+      e.target.remove();
+    }    
   }
 
-  //   for 'dragover' event on target (drop zone)
+  // for 'dragover' event on target (drop zone)
+  // e.preventDefault() required for drag'n'drop to work
   dragPictoOverDropZone(e) {
     e.preventDefault();
   }
 
   // for 'dragenter' and 'dragleave' events on target (drop zone)
-  highlightDropZone(e) {
-    e.preventDefault();
-    console.log("entering drop zone");
-    e.target.classList.add("bg-red");
+  highlightDropZone(e) {   
+    e.target.style.borderColor = "red";
   }
   dehighlightDropZone(e) {
-    e.preventDefault();
-    console.log("leaving drop zone");
-    e.target.classList.remove("bg-red");
+    e.dataTransfer.setData("targetParentId", "");
+    e.target.style.borderColor = "";
   }
 
   // for 'drop' event on target (drop zone)
   cloneAndDropPicto(e) {
     const img = document.createElement("img");
     img.src = e.dataTransfer.getData("image");
-    img.alt = e.dataTransfer.getData("alt");    
+    img.alt = e.dataTransfer.getData("alt");
     img.draggable = true;
 
     img.dataset.id = e.dataTransfer.getData("id");
-    // img.dataset.textToSpeech.title.param = e.dataTransfer.getData("title");
-    img.setAttribute("data-textToSpeech-title-param", e.dataTransfer.getData("title"));
-    img.classList.add("aspect-square", 'z-10');
+
+    img.setAttribute(
+      "data-textToSpeech-title-param",
+      e.dataTransfer.getData("title")
+    );
+    img.setAttribute("data-dragAndDrop-id-param", e.dataTransfer.getData("id"));
+    img.setAttribute(
+      "data-dragAndDrop-title-param",
+      e.dataTransfer.getData("title")
+    );
+    img.setAttribute(
+      "data-dragAndDrop-alt-param",
+      e.dataTransfer.getData("alt")
+    );
+    img.setAttribute(
+      "data-dragAndDrop-image-param",
+      e.dataTransfer.getData("image")
+    );
+
+    img.classList.add("aspect-square", "cursor-grab");
     img.dataset.controller = "dragAndDrop textToSpeech";
-    img.dataset.action = "dragstart->dragAndDrop#dragPicto dragend->dragAndDrop#testPicto click->textToSpeech#toSpeech";
+    img.dataset.action =
+      "dragstart->dragAndDrop#dragStartPicto dragend->dragAndDrop#dragEndPicto click->textToSpeech#toSpeech";
 
     e.target.appendChild(img);
-    console.log(img);
-    console.log("drop happens");
+    e.target.style.borderColor = "";
   }
 }
